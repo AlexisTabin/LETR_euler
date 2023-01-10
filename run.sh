20a9e06f6d6b875b6bd278b12448cef0236a62e1
Marco Voegeli <marvoegeli@eu-login-34.euler.ethz.ch>
2023-01-10 12:09:26 +0100

    changed script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 8
 #SBATCH --mem-per-cpu=40G
 #SBATCH --gpus-per-node=8
-#SBATCH --time=0-00:30:00
+#SBATCH --time=0-03:59:59
 
 #SBATCH --job-name=letr
 #SBATCH --output=letr.out
@@ -14,10 +14,7 @@ echo "Allocating 8 tasks on 8 nodes with 8 GPUs per Node"
 
 rm -rf exp/res50_stage1/
 
-conda activate deepl
-
 bash script/train/a0_train_stage1_res50.sh  res50_stage1
-
 echo "Done rendering"
 
 exit 0
