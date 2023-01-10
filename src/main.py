37d30a7e182ff499ebff088bd7fc297e83eb6d25
Marco Voegeli <alvin.mvoegeli@gmail.com>
2023-01-10 20:30:12 +0100

    🚧 removing pll

--- src/main.py
+++ src/main.py
@@ -38,9 +38,11 @@ def main(args):
     model.to(device)
 
     model_without_ddp = model
+    '''
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
+    '''
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
 
