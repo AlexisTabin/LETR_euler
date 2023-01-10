99cad161352fe704735c8f093fb997bc1f655b53
Alexis Tabin <atabin@eu-login-35.euler.ethz.ch>
2023-01-10 17:35:46 +0100

    almost workin

--- run.sh
+++ run.sh
@@ -7,15 +7,17 @@
 #SBATCH --gpus-per-node=8
 #SBATCH --time=0-03:59:59
 
-#SBATCH --job-name=letr
-#SBATCH --output=letr.out
-#SBATHC --error=letr.err
+#SBATCH --job-name=letr_wb
+#SBATCH --output=letr_wb.out
+#SBATCH --error=letr_wb.err
 
 echo "Allocating 8 tasks on 8 nodes with 8 GPUs per Node"
 
-rm -rf exp/res50_stage1/
-wandb login 
-bash script/train/a0_train_stage1_res50.sh  res50_stage1
+rm -rf exp/res50_stage1_wb/
+
+wandb login
+
+bash script/train/a0_train_stage1_res50.sh  res50_stage1_wb
 echo "Done rendering"
 
 exit 0
