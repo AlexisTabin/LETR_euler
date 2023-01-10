125e48d5f965ecd0d6f1e8d376db55cdc004b123
Marco Voegeli <alvin.mvoegeli@gmail.com>
2023-01-10 21:19:20 +0100

    🚧 omg json

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ import wandb
 
 def main(args):
     print("ARGS : ", args)
-    #utils.init_distributed_mode(args)
+    utils.init_distributed_mode(args)
     print("git:\n  {}\n".format(utils.get_sha()))
 
 
@@ -38,11 +38,10 @@ def main(args):
     model.to(device)
 
     model_without_ddp = model
-    '''
     if args.distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
-    '''
+
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print('number of params:', n_parameters)
 
