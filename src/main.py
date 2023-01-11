6405e016797732974281b32692aa486d58e9cb6f
AlexisTabin <atabin@student.ethz.ch>
2023-01-11 18:57:33 +0100

    remove unnecessary comment

--- src/main.py
+++ src/main.py
@@ -223,5 +223,4 @@ if __name__ == '__main__':
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
         Path(args.output_dir+'/checkpoints').mkdir(parents=True, exist_ok=True)
     
- #   print("yo args : ", args)
     main(args)
