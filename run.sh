e331e1ea2355d7ef859ac65893e7fc725c918be7
Alexis Tabin <atabin@eu-login-36.euler.ethz.ch>
2023-01-11 09:45:46 +0100

    alexis run

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-#SBATCH -n 1
-#SBATCH -N 1
+#SBATCH --ntasks=8
+#SBATCH --nodes=4
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=1
-#SBATCH --time=0-00:10:00
+#SBATCH --gpus-per-node=8
+#SBATCH --time=0-3:59:59
 
-#SBATCH --job-name=letr_wb
-#SBATCH --output=letr_wb.out
-#SBATCH --error=letr_wb.err
+#SBATCH --job-name=letr_848
+#SBATCH --output=letr_848.out
+#SBATCH --error=letr_848.err
+
+#SBATCH --mail-type=BEGIN    # notify on state change: BEGIN, END, FAIL or ALL
+#SBATCH --mail-user=$USER   # who to send email notification for job stats changes
 
 echo "Allocating 8 tasks on 4 nodes with 8 GPUs per Node"
 
-rm -rf exp/res50_stage1_wb/
+rm -rf exp/res50_stage1_848/
 
 wandb login
 
-bash script/train/a0_train_stage1_res50.sh  res50_stage1_wb
+bash script/train/a0_train_stage1_res50.sh  res50_stage1_848
 echo "Done rendering"
 
 exit 0
