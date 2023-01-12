a50ae7f49a3cd3c72ef87d5a4aa44d6f16c354e0
AlexisTabin <atabin@student.ethz.ch>
2023-01-12 21:38:19 +0100

    Running a1 from resume for 24h

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@
 #SBATCH --mem-per-cpu=40G
 
 #SBATCH --gpus-per-node=3
-#SBATCH --time=0-3:59:59
+#SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=4h_r101_213_s1
-#SBATCH --output=4h_r101_213_s1.out
-#SBATCH --error=4h_r101_213_s1.err
+#SBATCH --job-name=24h_r101_213_s1
+#SBATCH --output=24h_r101_213_s1.out
+#SBATCH --error=24h_r101_213_s1.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
@@ -17,9 +17,9 @@
 echo "Allocating 2 tasks on 1 nodes with 3 GPUs per Node"
 echo "Running A1"
 
-rm -rf exp/res101_stage1_213/
+rm -rf exp/res101_stage1_213_24h/
 
-bash script/train/a1_train_stage1_res101.sh  res101_stage1_213
+bash script/train/a1_train_stage1_res101.sh  res101_stage1_213_24h
 echo "Done rendering"
 
 exit 0
