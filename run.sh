adfd8ce7afc1980f766c11c1aa8fcf658b6abbb0
AlexisTabin <atabin@student.ethz.ch>
2023-01-12 10:57:12 +0100

    try

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #SBATCH --ntasks=8
-#SBATCH --nodes=8
+#SBATCH --nodes=1
 #SBATCH --mem-per-cpu=40G
 
 #SBATCH --gpus-per-node=8
-#SBATCH --time=0-11:59:59
+#SBATCH --time=0-3:59:59
 
-#SBATCH --job-name=resnet101_888
-#SBATCH --output=resnet101_888.out
-#SBATCH --error=resnet101_888.err
+#SBATCH --job-name=resnet101_818
+#SBATCH --output=resnet101_818.out
+#SBATCH --error=resnet101_818.err
 
-#SBATCH --mail-type=BEGIN    # notify on state change: BEGIN, END, FAIL or ALL
-#SBATCH --mail-user=$USER   # who to send email notification for job stats changes
+#SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
+#SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 8 tasks on 8 nodes with 8 GPUs per Node"
+echo "Allocating 8 tasks on 1 nodes with 8 GPUs per Node"
 echo "Running A1"
 
-rm -rf exp/res101_stage1_888/
+rm -rf exp/res101_stage2_818/
 
-bash script/train/a1_train_stage1_res101.sh  res101_stage1_888
+bash script/train/a2_train_stage2_res50.sh  res50_stage2_818
 echo "Done rendering"
 
 exit 0
