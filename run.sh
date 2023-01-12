d2f85d370cc560e0ff021fed489f4f1e6534833a
AlexisTabin <atabin@student.ethz.ch>
2023-01-12 11:40:17 +0100

    modifiy infos

--- run.sh
+++ run.sh
@@ -7,17 +7,17 @@
 #SBATCH --gpus-per-node=8
 #SBATCH --time=0-3:59:59
 
-#SBATCH --job-name=resnet101_818
-#SBATCH --output=resnet101_818.out
-#SBATCH --error=resnet101_818.err
+#SBATCH --job-name=r50_818_s2
+#SBATCH --output=r50_818_s2.out
+#SBATCH --error=r50_818_s2.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
 echo "Allocating 8 tasks on 1 nodes with 8 GPUs per Node"
-echo "Running A1"
+echo "Running A2"
 
-rm -rf exp/res101_stage2_818/
+rm -rf exp/res50_stage2_818/
 
 bash script/train/a2_train_stage2_res50.sh  res50_stage2_818
 echo "Done rendering"
