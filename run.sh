1df338670971cca4b2e606d5a5dda6b4e4a9a45c
AlexisTabin <atabin@student.ethz.ch>
2023-01-12 13:51:02 +0100

    reduce gpus per node

--- run.sh
+++ run.sh
@@ -4,21 +4,21 @@
 #SBATCH --nodes=2
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=8
+#SBATCH --gpus-per-node=5
 #SBATCH --time=0-3:59:59
 
-#SBATCH --job-name=r50_828_s2
-#SBATCH --output=r50_828_s2.out
-#SBATCH --error=r50_828_s2.err
+#SBATCH --job-name=r50_825_s2
+#SBATCH --output=r50_825_s2.out
+#SBATCH --error=r50_825_s2.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 8 tasks on 2 nodes with 8 GPUs per Node"
+echo "Allocating 8 tasks on 2 nodes with 5 GPUs per Node"
 echo "Running A2"
 
-rm -rf exp/res50_stage2_828/
-bash script/train/a2_train_stage2_res50.sh  res50_stage2_828
+rm -rf exp/res50_stage2_825/
+bash script/train/a2_train_stage2_res50.sh  res50_stage2_825
 echo "Done rendering"
 
 exit 0
