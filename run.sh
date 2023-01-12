f8de8bd0ee08ef0781c01c18307b53e88cfce62d
Alexis Tabin <atabin@eu-login-07.euler.ethz.ch>
2023-01-12 10:02:47 +0100

    running res101 and a2

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 #SBATCH --ntasks=8
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 #SBATCH --mem-per-cpu=40G
 
 #SBATCH --gpus-per-node=8
-#SBATCH --time=0-3:59:59
+#SBATCH --time=0-11:59:59
 
-#SBATCH --job-name=letr_848
-#SBATCH --output=letr_848.out
-#SBATCH --error=letr_848.err
+#SBATCH --job-name=resnet101_888
+#SBATCH --output=resnet101_888.out
+#SBATCH --error=resnet101_888.err
 
 #SBATCH --mail-type=BEGIN    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=$USER   # who to send email notification for job stats changes
 
-echo "Allocating 8 tasks on 4 nodes with 8 GPUs per Node"
+echo "Allocating 8 tasks on 8 nodes with 8 GPUs per Node"
+echo "Running A1"
 
-rm -rf exp/res50_stage1_848/
+rm -rf exp/res101_stage1_888/
 
-wandb login
-
-bash script/train/a0_train_stage1_res50.sh  res50_stage1_848
+bash script/train/a1_train_stage1_res101.sh  res101_stage1_888
 echo "Done rendering"
 
 exit 0
