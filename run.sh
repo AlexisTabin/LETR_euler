7dead427e24a6d7c74396144ee67429673bd20cd
AlexisTabin <atabin@student.ethz.ch>
2023-01-13 23:34:43 +0100

    12 tasks 2 nodes 7 gpp

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#SBATCH --ntasks=3
-#SBATCH --nodes=1
+#SBATCH --ntasks=12
+#SBATCH --nodes=2
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=4
-#SBATCH --time=0-3:59:59
+#SBATCH --gpus-per-node=7
+#SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=3h_r50_314_s2
-#SBATCH --output=4h_r50_314_s2.out
-#SBATCH --error=4h_r50_314_s2.err
+#SBATCH --job-name=r101_12_2_7_a3_24h
+#SBATCH --output=r101_12_2_7_a3_24h.out
+#SBATCH --error=r101_12_2_7_a3_24h.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 3 tasks on 1 nodes with 4 GPUs per Node for 4h"
-echo "Running A2"
+echo "Allocating 12 tasks on 2 nodes with 7 GPUs per Node for 24h"
+echo "Running A3"
 
-rm -rf exp/res50_stage2_314_4h/
-bash script/train/a2_train_stage2_res50.sh  res50_stage2_314_4h
+rm -rf exp/res101_stage2_12_2_7/
+bash script/train/a3_train_stage2_res101.sh  res101_stage2_12_2_7
 echo "Done rendering"
 
 exit 0
