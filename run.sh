19e230ca6330c496313021bbc96659cee863f7e4
AlexisTabin <atabin@student.ethz.ch>
2023-01-13 23:27:49 +0100

    a2 617

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#SBATCH --ntasks=3
+#SBATCH --ntasks=6
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=4
-#SBATCH --time=0-3:59:59
+#SBATCH --gpus-per-node=7
+#SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=3h_r50_314_s2_resume
-#SBATCH --output=4h_r50_314_s2_resume.out
-#SBATCH --error=4h_r50_314_s2_resume.err
+#SBATCH --job-name=24h_r50_617_a2
+#SBATCH --output=24h_r50_617_a2.out
+#SBATCH --error=24h_r50_617_a2.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 3 tasks on 1 nodes with 4 GPUs per Node for 4h from resume"
+echo "Allocating 6 tasks on 1 nodes with 7 GPUs per Node for 24h from resume"
 echo "Running A2 from resume"
 
-rm -rf exp/res50_stage2_314_4h_resumed/
-bash script/train/a2_train_stage2_res50.sh  res50_stage2_314_4h_resumed
+rm -rf exp/res50_stage2_617_24h/
+bash script/train/a2_train_stage2_res50.sh  res50_stage2_617_24h
 echo "Done rendering"
 
 exit 0
