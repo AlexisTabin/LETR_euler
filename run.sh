09d2bbd394ba13ee1a77520d514829d52a066905
AlexisTabin <atabin@student.ethz.ch>
2023-01-13 08:38:50 +0100

    32 - 4 -8

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#SBATCH --ntasks=2
-#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --nodes=4
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=3
+#SBATCH --gpus-per-node=8
 #SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=24h_r101_213_s1
-#SBATCH --output=24h_r101_213_s1.out
-#SBATCH --error=24h_r101_213_s1.err
+#SBATCH --job-name=24h_r101_32_48_a1
+#SBATCH --output=24h_r101_32_48_a1.out
+#SBATCH --error=24h_r101_32_48_a1.err
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 2 tasks on 1 nodes with 3 GPUs per Node"
+echo "Allocating 32 tasks on 4 nodes with 8 GPUs per Node"
 echo "Running A1"
 
-rm -rf exp/res101_stage1_213_24h/
+rm -rf exp/res101_stage1_32_48_24h/
 
-bash script/train/a1_train_stage1_res101.sh  res101_stage1_213_24h
+bash script/train/a1_train_stage1_res101.sh  res101_stage1_32_48_24h
 echo "Done rendering"
 
 exit 0
