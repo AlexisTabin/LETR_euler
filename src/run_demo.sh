5485c9781c3000067988ed4e6f4add0b68d16097
AlexisTabin <atabin@student.ethz.ch>
2023-01-14 14:57:46 +0100

    copy latest checkpoint before plotting demo

--- src/run_demo.sh
+++ src/run_demo.sh
@@ -10,10 +10,17 @@
 #SBATCH --job-name=demo
 #SBATCH --output=demo.out
 
-#SBATCH --mail-type=BEGIN    
-#SBATCH --mail-user=$USER   
 
 echo "Running demo"
+
+
+EXP_PATH=/cluster/scratch/atabin/LETR_euler/exp
+echo "EXP_PATH: $EXP_PATH"
+echo "Copying checkpoints to $EXP_PATH/checkpoints"
+cp $EXP_PATH/res50_stage1_415_24h/checkpoints/checkpoint.pth $EXP_PATH/checkpoints/checkpoint_res50_s1.pth
+cp $EXP_PATH/res50_stage2_617_24h/checkpoints/checkpoint.pth $EXP_PATH/checkpoints/checkpoint_res50_s2.pth
+cp $EXP_PATH/res101_stage1_718_24h/checkpoints/checkpoint.pth $EXP_PATH/checkpoints/checkpoint_res101_s1.pth
+cp $EXP_PATH/res101_stage2_415/checkpoints/checkpoint.pth $EXP_PATH/checkpoints/checkpoint_res101_s2.pth
 
 python3 demo.py
 
