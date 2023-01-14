20199de03bd1205646d0595e65259c15de2b5a0f
AlexisTabin <atabin@student.ethz.ch>
2023-01-14 08:03:47 +0100

    Wrong data path

--- script/train/a3_train_stage2_res101.sh
+++ script/train/a3_train_stage2_res101.sh
@@ -18,7 +18,7 @@ if [ ! -d "$output"  ]; then
     cp $0 $output/run.bash
 
     PYTHONPATH=$PYTHONPATH:./src python -m torch.distributed.launch \
-    --master_port=$((1000 + RANDOM % 9999)) --nproc_per_node=4 --use_env  src/main.py --coco_path data/wireframe_processed --wandb_name $name \
+    --master_port=$((1000 + RANDOM % 9999)) --nproc_per_node=4 --use_env  src/main.py --coco_path data_normals/ --wandb_name $name \
     --output_dir $output --LETRpost --backbone resnet101 --layer1_frozen --frozen_weights exp/res101_stage1/checkpoints/checkpoint.pth --no_opt \
     --batch_size 1 --epochs 300 --lr_drop 120 --num_queries 1000 --num_gpus 4 | tee -a $output/history.txt \
 
