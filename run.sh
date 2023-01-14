71925e4b148740318c7085930c6dccf37543c8f8
AlexisTabin <atabin@student.ethz.ch>
2023-01-14 15:29:09 +0100

    a2 from a0 with 200 epochs

--- run.sh
+++ run.sh
@@ -7,18 +7,17 @@
 #SBATCH --gpus-per-node=7
 #SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=24h_r50_617_a2
-#SBATCH --output=24h_r50_617_a2.out
-#SBATCH --error=24h_r50_617_a2.err
+#SBATCH --job-name=a2
+#SBATCH --output=a2.out
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-echo "Allocating 6 tasks on 1 nodes with 7 GPUs per Node for 24h from resume"
-echo "Running A2 from resume"
+echo "Allocating 6 tasks on 1 nodes with 7 GPUs per Node for 24h from a0 with 200 epochs"
+echo "Running A2"
 
-rm -rf exp/res50_stage2_617_24h/
-bash script/train/a2_train_stage2_res50.sh  res50_stage2_617_24h
+rm -rf exp/res50_stage2_from_a0_with_200_epochs/
+bash script/train/a2_train_stage2_res50.sh  res50_stage2_from_a0_with_200_epochs
 echo "Done rendering"
 
 exit 0
