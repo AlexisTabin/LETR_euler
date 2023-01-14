92a8f28c1e17b25ed8bf12a060f270d6ced76816
AlexisTabin <atabin@student.ethz.ch>
2023-01-14 18:13:25 +0100

    a3 516

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-#SBATCH --ntasks=4
+#SBATCH --ntasks=5
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=40G
 
-#SBATCH --gpus-per-node=5
+#SBATCH --gpus-per-node=6
 #SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=r101_415_a3_24h
-#SBATCH --output=r101_415_a3_24h.out
-#SBATCH --error=r101_415_a3_24h.err
+#SBATCH --job-name=a3
+#SBATCH --output=a3.out
 
 #SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
 #SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
@@ -17,8 +16,10 @@
 echo "Allocating 12 tasks on 2 nodes with 7 GPUs per Node for 24h"
 echo "Running A3"
 
-rm -rf exp/res101_stage2_415/
-bash script/train/a3_train_stage2_res101.sh  res101_stage2_415
+cp exp/res101_stage1_718_24h/checkpoints/checkpoint.pth exp/res101_stage1/checkpoints/checkpoint.pth
+
+rm -rf exp/res101_stage2_415_from_170_epochs/
+bash script/train/a3_train_stage2_res101.sh  res101_stage2_415_from_170_epochs
 echo "Done rendering"
 
 exit 0
