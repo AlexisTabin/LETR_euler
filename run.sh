51da1180fec0f670584f1284954e7c05ff12f188
AlexisTabin <atabin@student.ethz.ch>
2023-01-15 18:48:25 +0100

    3

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --ntasks=2
+#SBATCH --ntasks=3
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=3300m
 
-#SBATCH --gpus-per-node=3
+#SBATCH --gpus-per-node=4
 #SBATCH --gres=gpumem:20g
 #SBATCH --time=0-23:59:59
 
