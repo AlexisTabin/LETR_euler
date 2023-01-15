ebeed7adf238b9c2cf3cfca539a5f3db1c7a9a51
AlexisTabin <atabin@student.ethz.ch>
2023-01-15 22:01:50 +0100

    first try

--- LETR/src/demo_letr.py
+++ LETR/src/demo_letr.py
@@ -7,6 +7,7 @@ import torchvision.transforms.functional as functional
 import torch.nn.functional as F
 from models import build_model
 from util.misc import nested_tensor_from_tensor_list
+import os
 
 class Compose(object):
     def __init__(self, transforms):
@@ -78,61 +79,67 @@ class Resize(object):
         size = self.sizes
         return resize(img, size, self.max_size)
 
-# obtain checkpoints
-checkpoint = torch.load('../exp/res101_stage2_focal/checkpoints/checkpoint0024.pth', map_location='cpu')
+checkpoints_path = '../../exp/letr_chkpt/'
 
-# load model
-args = checkpoint['args']
-model, _, postprocessors = build_model(args)
-model.load_state_dict(checkpoint['model'])
-model.eval()
+def infer_letr(checkpoint):
+    model_path = checkpoints_path + checkpoint
+    # obtain checkpoints
+    checkpoint = torch.load(model_path, map_location='cpu')
 
-# load image
-raw_img = plt.imread('../figures/demo.png')
-h, w = raw_img.shape[0], raw_img.shape[1]
-orig_size = torch.as_tensor([int(h), int(w)])
+    # load model
+    args = checkpoint['args']
+    model, _, _ = build_model(args)
+    model.load_state_dict(checkpoint['model'])
+    epochs = checkpoint['epoch']
+    model.eval()
 
-# normalize image
-test_size = 1100
-normalize = Compose([
-        ToTensor(),
-        Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
-        Resize([test_size]),
-    ])
-img = normalize(raw_img)
-inputs = nested_tensor_from_tensor_list([img])
-plt.axis('off')
-plt.imshow(raw_img)
+    # load image
+    raw_img = plt.imread('../figures/demo.png')
+    h, w = raw_img.shape[0], raw_img.shape[1]
+    orig_size = torch.as_tensor([int(h), int(w)])
 
+    # normalize image
+    test_size = 1100
+    normalize = Compose([
+            ToTensor(),
+            Normalize([0.538, 0.494, 0.453], [0.257, 0.263, 0.273]),
+            Resize([test_size]),
+        ])
+    img = normalize(raw_img)
+    inputs = nested_tensor_from_tensor_list([img])
+    plt.axis('off')
+    plt.imshow(raw_img)
 
-outputs = model(inputs)[0]
+    outputs = model(inputs)[0]
 
-out_logits, out_line = outputs['pred_logits'], outputs['pred_lines']
-prob = F.softmax(out_logits, -1)
-scores, labels = prob[..., :-1].max(-1)
-img_h, img_w = orig_size.unbind(0)
-scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
-lines = out_line * scale_fct[:, None, :]
-lines = lines.view(1000, 2, 2)
-lines = lines.flip([-1])# this is yxyx format
-scores = scores.detach().numpy()
-keep = scores >= 0.7
-keep = keep.squeeze()
-lines = lines[keep]
-lines = lines.reshape(lines.shape[0], -1)
+    out_logits, out_line = outputs['pred_logits'], outputs['pred_lines']
+    prob = F.softmax(out_logits, -1)
+    scores, _ = prob[..., :-1].max(-1)
+    img_h, img_w = orig_size.unbind(0)
+    scale_fct = torch.unsqueeze(torch.stack([img_w, img_h, img_w, img_h], dim=0), dim=0)
+    lines = out_line * scale_fct[:, None, :]
+    lines = lines.view(1000, 2, 2)
+    lines = lines.flip([-1])# this is yxyx format
+    scores = scores.detach().numpy()
+    keep = scores >= 0.7
+    keep = keep.squeeze()
+    lines = lines[keep]
+    lines = lines.reshape(lines.shape[0], -1)
 
+    fig = plt.figure()
+    plt.imshow(raw_img)
+    for line in lines:
+        y1, x1, y2, x2 = line # this is yxyx
+        p1 = (x1, y1)
+        p2 = (x2, y2)
+        plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
+    plt.axis('off')
 
+    title = checkpoint.split('.')[0] + f'_w_epochs_{epochs}'
+    plt.savefig(f"../figures/{title}.png", dpi=300, bbox_inches='tight', pad_inches = 0)
+    plt.close(fig)
 
-fig = plt.figure()
-plt.imshow(raw_img)
-for tp_id, line in enumerate(lines):
-    y1, x1, y2, x2 = line # this is yxyx
-    p1 = (x1, y1)
-    p2 = (x2, y2)
-    plt.plot([p1[0], p2[0]], [p1[1], p2[1]], linewidth=1.5, color='darkorange', zorder=1)
-plt.axis('off')
-
-
-#plt.savefig("../figures/demo_result.png", dpi=300, bbox_inches='tight', pad_inches = 0)
-#plt.close(fig)
-plt.show()
+def main():
+    checkpoints = os.listdir(checkpoints_path)
+    for checkpoint in checkpoints:
+        infer_letr(checkpoint)
