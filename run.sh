67c605629414e920d229d3e3ed6120a3eb025703
AlexisTabin <atabin@student.ethz.ch>
2023-01-15 18:19:52 +0100

    update

--- run.sh
+++ run.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-#SBATCH --ntasks=8
-#SBATCH --nodes=4
-#SBATCH --mem-per-cpu=40G
+#SBATCH --ntasks=6
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=3300m
 
-#SBATCH --gpus-per-node=8
-#SBATCH --time=0-3:59:59
+#SBATCH --gpus-per-node=7
+#SBATCH --gres=gpumem:20g
+#SBATCH --time=0-23:59:59
 
-#SBATCH --job-name=letr_848
-#SBATCH --output=letr_848.out
-#SBATCH --error=letr_848.err
 
-#SBATCH --mail-type=BEGIN    # notify on state change: BEGIN, END, FAIL or ALL
-#SBATCH --mail-user=$USER   # who to send email notification for job stats changes
+#SBATCH --job-name=a4
+#SBATCH --output=a4.out
 
-echo "Allocating 8 tasks on 4 nodes with 8 GPUs per Node"
+#SBATCH --mail-type=ALL    # notify on state change: BEGIN, END, FAIL or ALL
+#SBATCH --mail-user=atabin@student.ethz.ch  # who to send email notification for job stats changes
 
-rm -rf exp/res50_stage1_848/
+echo "Running A4"
 
-wandb login
-
-bash script/train/a0_train_stage1_res50.sh  res50_stage1_848
+rm -rf exp/a4/
+bash script/train/a4_train_stage2_focal_res50.sh  a4
 echo "Done rendering"
 
 exit 0
