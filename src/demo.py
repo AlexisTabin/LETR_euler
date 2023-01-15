79335b6d8461574ad7d0490620ce86c306016b8e
AlexisTabin <atabin@student.ethz.ch>
2023-01-15 17:04:35 +0100

    test

--- src/demo.py
+++ src/demo.py
@@ -87,7 +87,7 @@ class Resize(object):
         return resize(img, size, self.max_size)
 
 
-def infer_on_image(model_name, raw_img):#, ax):
+def infer_on_image(model_name, raw_img, ax):
     model_path = MODELS_DIR + model_name
     model_name_without_pth = model_name.split('.')[0]
     title = ' '.join(model_name_without_pth.split('_')[1:])
@@ -157,9 +157,9 @@ def infer_on_image(model_name, raw_img):#, ax):
     fig_width = 20
     fig_height = fig_width * ratio
 
-    fig, ax = plt.subplots(1, 1, figsize=(fig_width, fig_height), frameon=False)
+    # fig, ax = plt.subplots(1, 1, figsize=(fig_width, fig_height), frameon=False)
     ax.axis('off')
-    # ax.set_title(title)
+    ax.set_title(title)
     title = title + '\n ({} epochs)'.format(epochs)
     ax.imshow(raw_img)
     for tp_id, line in enumerate(lines_text):
@@ -170,9 +170,9 @@ def infer_on_image(model_name, raw_img):#, ax):
         ax.plot([p1[0], p2[0]], [p1[1], p2[1]],
                  linewidth=1.5, color=temp_color, zorder=1)
 
-    plt.subplots_adjust(hspace=0, wspace=0)
-    fig.savefig(os.path.join('demo/', model_name_without_pth + '.png'), dpi=300, bbox_inches='tight', pad_inches=0)
-    plt.close(fig)
+    # plt.subplots_adjust(hspace=0, wspace=0)
+    # fig.savefig(os.path.join('demo/', model_name_without_pth + '.png'), dpi=300, bbox_inches='tight', pad_inches=0)
+    # plt.close(fig)
 
 def main():
     # (deepl) scp atabin@euler.ethz.ch:/cluster/scratch/atabin/LETR_euler/exp/res50_stage1_415_24h/checkpoints/checkpoint.pth ./exp/checkpoints/checkpoint_res50_s1.pth
@@ -185,25 +185,25 @@ def main():
     ratio = img_h / img_w
 
     # compute figure size in function of image size
-    fig_width = 20
-    fig_height = fig_width * ratio
+    fig_width = img_h*2 + 6
+    fig_height = img_h*3 + 9
 
     # 2 columns, 4 rows
     # first column for resnet50, second for resnet101
     # first row for input image, second for stage1, third for stage2, fourth for stage3    
-    # fig, axes = plt.subplots(4, 2, figsize=(fig_width, fig_height))
-    # fig.suptitle('Demo')
+    fig, axes = plt.subplots(4, 2, figsize=(fig_width, fig_height))
+    fig.suptitle('Demo')
 
-    # axes[0][0].imshow(raw_img)
-    # axes[0][0].axis('off')
-    # axes[0][0].set_title('Input Image')
-    # axes[0][1].imshow(raw_img)
-    # axes[0][1].axis('off')
-    # axes[0][1].set_title('GT')
+    axes[0][0].imshow(raw_img)
+    axes[0][0].axis('off')
+    axes[0][0].set_title('Input Image')
+    axes[0][1].imshow(raw_img)
+    axes[0][1].axis('off')
+    axes[0][1].set_title('GT')
 
-    # axes[3][0].axis('off')
-    # axes[3][1].axis('off')
-    # axes[3][1].figure.savefig(os.path.join('demo/', 'test'+ '.png'), dpi=300)
+    axes[3][0].axis('off')
+    axes[3][1].axis('off')
+    axes[3][1].figure.savefig(os.path.join('demo/', 'test'+ '.png'), dpi=300)
 
 
     print('Running inference on image', flush=True)
@@ -224,11 +224,11 @@ def main():
         print("Row   : ", row)
         print("Column: ", column)
 
-        # ax = axes[row][column]
-        infer_on_image(model, raw_img)#, ax)
+        ax = axes[row][column]
+        infer_on_image(model, raw_img, ax)
 
     # plt.show()
     print('Saving results', flush=True)
-    plt.savefig('demo.png')
+    plt.savefig('demo.png', bbox_inches='tight', pad_inches=0)
 
 main()
