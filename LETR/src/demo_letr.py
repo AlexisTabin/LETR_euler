9cd06a8fbb4bb84f168ed34767db2fd81e6d0b77
AlexisTabin <atabin@student.ethz.ch>
2023-01-16 16:59:16 +0100

    re

--- LETR/src/demo_letr.py
+++ LETR/src/demo_letr.py
@@ -136,7 +136,7 @@ def infer_letr(checkpoint_name, demo_name):
         y1, x1, y2, x2 = line # this is yxyx
         p1 = (x1, y1)
         p2 = (x2, y2)
-        plt.plot([p1[0].detach().numpy(), p2[0].detach().numpy()], [p1[1].detach().numpy(), p2[1].detach().numpy()], linewidth=1.5, color='blue', zorder=1)
+        plt.plot([p1[0].detach().numpy(), p2[0].detach().numpy()], [p1[1].detach().numpy(), p2[1].detach().numpy()], linewidth=1.5, color='darkorange', zorder=1)
     plt.axis('off')
 
     title = checkpoint_name.split('.')[0] + f'_w_epochs_{epochs}'
